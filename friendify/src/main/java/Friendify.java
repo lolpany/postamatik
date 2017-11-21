4d23ac30d31d3b4580993b72a2dd2e3d8d64b603
user <gbesergey@gmail.com>
2017-11-22 00:19:44 +0300

    update friendify to modern selenide api

--- friendify/src/main/java/Friendify.java
+++ friendify/src/main/java/Friendify.java
@@ -1,27 +1,28 @@
-package lol.lolpany;
-
-import com.codeborne.selenide.Configuration;
-import org.openqa.selenium.By;
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.ElementsCollection;
+import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.SelenideElement;
 import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.firefox.FirefoxProfile;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
-import javax.mail.*;
+import javax.mail.Folder;
+import javax.mail.Message;
+import javax.mail.Session;
+import javax.mail.Store;
 import java.io.IOException;
 import java.util.Date;
-import java.util.List;
 import java.util.Properties;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-import static com.codeborne.selenide.Selenide.sleep;
+import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.WebDriverRunner.getWebDriver;
+import static com.codeborne.selenide.WebDriverRunner.setWebDriver;
 import static java.util.concurrent.TimeUnit.MINUTES;
 import static java.util.concurrent.TimeUnit.NANOSECONDS;
-import static javax.mail.Folder.READ_ONLY;
 
 public class Friendify {
 
@@ -30,7 +31,7 @@ public class Friendify {
     private static final Pattern PIN_PATTERN = Pattern.compile("Please use this verification code to complete your sign in: (\\d+)");
 
 
-//    @Test
+    //    @Test
     public static void main(String[] args) throws Exception {
         int startPage = 1;
         int endPage = 200;
@@ -49,57 +50,50 @@ public class Friendify {
 
         FirefoxOptions firefoxOptions = new FirefoxOptions();
         firefoxOptions.setProfile(profile);
+//        firefoxOptions.addArguments("-headless");
         firefoxOptions.addCapabilities(capabilities);
 
-        WebDriver driver = new FirefoxDriver(firefoxOptions);
-        Configuration.baseUrl = "https://www.linkedin.com/";
+        setWebDriver(new FirefoxDriver(firefoxOptions));
+//        Configuration.baseUrl = "https://www.linkedin.com/";
 
-        if (!login(driver)) {
+        if (!login()) {
             throw new Exception();
         }
-        sleep(20000);
+        Selenide.sleep(20000);
 
         for (int i = startPage; i < endPage; i++) {
             try {
                 System.out.println(i);
-                driver.get("https://www.linkedin.com/search/results/people/?facetGeoRegion=%5B%22us%3A0%22%5D&facetNetwork=%5B%22S%22%5D&origin=FACETED_SEARCH&page=" + i);
-                JavascriptExecutor jse = (JavascriptExecutor) driver;
+                open("https://www.linkedin.com/search/results/people/?facetGeoRegion=%5B%22us%3A0%22%5D&facetNetwork=%5B%22S%22%5D&origin=FACETED_SEARCH&page=" + i);
+                JavascriptExecutor jse = (JavascriptExecutor) getWebDriver();
 
 //
 //            new WebDriverWait(driver, 10).until((ExpectedCondition<Boolean>) wd ->
 //                    ((JavascriptExecutor) wd).executeScript("return document.readyState").equals("complete"));
 
-                sleep(15000);
+                Selenide.sleep(15000);
 
                 jse.executeScript("window.scrollBy(0,500)", "");
 
-                sleep(5000);
+                Selenide.sleep(5000);
 
 
-                List<WebElement> buttons = driver.findElements(By.className("search-result__actions--primary"));
+                ElementsCollection buttons = $$(".search-result__actions--primary");
 
 
                 for (int j = 0; j < buttons.size(); j++) {
                     jse.executeScript("window.scrollBy(0,1000)", "");
-                    WebElement button = driver.findElements(By.className("search-result__actions--primary")).get(j);
+                    SelenideElement button = $$("button.search-result__actions--primary").get(j);
                     if (button != null) {
                         System.out.println(button.getText());
                         if ("Connect".equals(button.getText())) {
                             try {
-                                button.click();
-                                WebElement connectDiaglog = null;
-                                while (connectDiaglog == null) {
-                                    connectDiaglog = driver.findElement(By.className("modal-wormhole-content"));
-                                }
+                                button.should(Condition.visible).click();
 
-                                WebElement sendButton = null;
-                                while (sendButton == null) {
-                                    sendButton = connectDiaglog.findElement(By.className("send-invite__actions"))
-                                            .findElement(By.className("button-primary-large"))
-                                    ;
-                                }
-                                sendButton.submit();
-                                sleep(5000);
+                                SelenideElement sendButton =
+                                        $("div.modal-wormhole-content div.send-invite__actions button.button-primary-large");
+                                sendButton.should(Condition.visible).click();
+//                                Selenide.sleep(5000);
                             } catch (Exception e) {
                                 e.printStackTrace();
                             }
@@ -113,16 +107,16 @@ public class Friendify {
         }
     }
 
-    public static boolean login(WebDriver driver) throws MessagingException, IOException {
-        driver.get("https://www.linkedin.com/uas/login");
-        driver.findElement(By.id("session_key-login")).sendKeys("gbesergey@gmail.com");
-        driver.findElement(By.id("session_password-login")).sendKeys("!@#$f23$Gfdfs3");
-        driver.findElement(By.id("btn-primary")).submit();
-        if (!driver.findElements(By.id("pagekey-uas-consumer-ato-pin-challenge")).isEmpty()) {
+    public static boolean login() throws IOException, javax.mail.MessagingException {
+        open("https://www.linkedin.com/uas/login");
+        $("#session_key-login").sendKeys("gbesergey@gmail.com");
+        $("#session_password-login").sendKeys("!@#$f23$Gfdfs3");
+        $("#btn-primary").submit();
+        if (!$$("#pagekey-uas-consumer-ato-pin-challenge").isEmpty()) {
             String pin = readVerificationCodeFromMail();
             if (pin != null) {
-                driver.findElement(By.id("verification-code")).sendKeys(pin);
-                driver.findElement(By.id("btn-primary")).submit();
+                $("#verification-code").sendKeys(pin);
+                $("#btn-primary").submit();
             } else {
                 return false;
             }
@@ -130,7 +124,7 @@ public class Friendify {
         return true;
     }
 
-    public static String readVerificationCodeFromMail() throws MessagingException, IOException {
+    public static String readVerificationCodeFromMail() throws IOException, javax.mail.MessagingException {
         Properties props = System.getProperties();
         props.setProperty("mail.store.protocol", "imaps");
         Session session = Session.getDefaultInstance(props, null);
@@ -139,7 +133,7 @@ public class Friendify {
 
         Folder inbox = store.getFolder("Inbox");
         try {
-            inbox.open(READ_ONLY);
+            inbox.open(Folder.READ_ONLY);
             while (true) {
                 long now = new Date().getTime();
                 for (Message message : inbox.getMessages()) {
